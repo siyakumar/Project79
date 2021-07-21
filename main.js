7e14a09bcaec2427955143d2fc529609071eb8a9
siyakumar <85111217+siyakumar@users.noreply.github.com>
2021-07-21 09:32:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+menu_list_array = ["Veg Margherita Pizza",//add more items  
+ "Chicken Tandoori Pizza","Veg Supreme Pizza","Paneer Tikka Pizza"                   ];
+
+function getmenu(){
+var htmldata;
+//Complete the code
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
